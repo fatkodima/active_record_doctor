c388e9744938a9d4bc0ca771d6edb5b901a5da2b
fatkodima <fatkodima123@gmail.com>
2025-03-26 23:08:22 +0200

    Make checkers agnostic of validator attributes types

--- lib/active_record_doctor/detectors/incorrect_length_validation.rb
+++ lib/active_record_doctor/detectors/incorrect_length_validation.rb
@@ -34,10 +34,10 @@ module ActiveRecordDoctor
         each_model(except: config(:ignore_models), existing_tables_only: true) do |model|
           each_attribute(model, except: config(:ignore_attributes), type: [:string, :text]) do |column|
             table = model.table_name
-            model_maximum = maximum_allowed_by_length_validation(model, column.name.to_sym)
+            model_maximum = maximum_allowed_by_length_validation(model, column.name)
             database_maximum = column_limit(table, column)
             next if model_maximum == database_maximum
-            next if column.limit && covered_by_inclusion_validation?(model, column.name.to_sym, database_maximum)
+            next if column.limit && covered_by_inclusion_validation?(model, column.name, database_maximum)
 
             problem!(
               model: model.name,
@@ -52,17 +52,21 @@ module ActiveRecordDoctor
 
       def maximum_allowed_by_length_validation(model, column)
         length_validator = model.validators.find do |validator|
+          attributes = validator.attributes.map(&:to_s)
+
           validator.kind == :length &&
             validator.options.include?(:maximum) &&
-            validator.attributes.include?(column)
+            attributes.include?(column)
         end
         length_validator ? length_validator.options[:maximum] : nil
       end
 
       def covered_by_inclusion_validation?(model, column, limit)
         inclusion_validator = model.validators.find do |validator|
+          attributes = validator.attributes.map(&:to_s)
+
           validator.kind == :inclusion &&
-            validator.attributes.include?(column)
+            attributes.include?(column)
         end
 
         if inclusion_validator
